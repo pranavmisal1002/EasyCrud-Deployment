75bf833d0a02bbda1a5d32a82ad24a81f2f85474
Pranav Misal <pranavmisal1002@gmail.com>
2026-02-26 15:04:52 +0530

    Create frontend-jenkins-file.groovy

---
+++ jenkinsfile/frontend-jenkins-file.groovy
@@ -0,0 +1,69 @@
+pipeline{
+    agent any
+    environment{
+        image_name_frontend = "pranavmisal1002/easy-crud-frontend"
+        tag = "v1"
+    }
+    stages{
+        stage('git pull'){
+            steps{
+              git branch: 'main', url: 'https://github.com/pranavmisal1002/EasyCRUD-Updated.git'  
+            
+            }
+        }
+        stage('Build Frontend'){
+            steps{
+                dir('frontend'){
+                sh '''docker rmi -f ${image_name_frontend}:${tag} || true
+                        docker build --no-cache -t ${image_name_frontend}:${tag} .
+                       docker images '''
+                }
+            }
+        }
+        stage('Push Image Hub'){
+            steps{
+                withCredentials([usernamePassword(credentialsId: 'Docker', passwordVariable: 'Docker_Password', usernameVariable: 'Docker_user')]) {
+                sh '''echo Logging into DockerHub...
+                      echo \$Docker_Password | docker login -u \$Docker_user --password-stdin
+                      docker push ${image_name_frontend}:${tag} 
+                      docker logout '''
+                }
+            }
+        }
+        stage('Configure Kube') {
+            steps {
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh '''
+                        aws eks update-kubeconfig --region eu-north-1 --name EKS-cluster
+                    '''
+                }
+            }
+        }
+        stage('K8s Deployment'){
+            steps{
+                dir('kubernetes'){
+                    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                        sh ''' kubectl apply -f frontend-deployment.yml'''
+                    }
+                }
+            }
+        }
+        stage('View pod & service'){
+            steps{
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                sh ''' kubectl get pods -o wide
+                       echo ########################################################
+                       kubectl get svc'''
+                }
+            }
+        }
+        stage('view backend LB URL'){
+            steps{
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                sh ''' echo Copy the LoadBalancer DNS and open in your browser
+                    kubectl get svc frontend-svc'''
+                }
+            }
+        } 
+    }
+}
